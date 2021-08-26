93e3ca0e4fb260bb1779a19e97e19cc8c38d5209
Tienducvi <ducvi0987@gmail.com>
2021-08-25 18:30:31 -0700

    file chua router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
+import HomeHeader from './containers/Header';
+import FooterHome from './containers/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <HomeHeader />
       <App />
+      <FooterHome />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
