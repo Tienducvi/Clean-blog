1e8a68fb1b86b4bb3dce463beb4ab89fafdc699a
Tienducvi <ducvi0987@gmail.com>
2021-09-06 07:36:41 -0700

    sua detail post

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { detailInfo } from '../redux/actions/productActions';
+import { useDispatch, useSelector } from 'react-redux';
+import { detailInfo } from '../redux/actions/productActions';
 import HomeHeader from './Header';
 import FooterHome from './Footer';
 
@@ -13,7 +13,7 @@ const api = axios.create({
 export default function PostDetail() {
   // eslint-disable-next-line prefer-const
   const { postId } = useParams();
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     api.get('/').then((res) => {
@@ -22,15 +22,17 @@ export default function PostDetail() {
     });
   }, []);
   const post = posts.find((pId) => pId.id === postId);
+  dispatch(detailInfo(post));
+  const postData = useSelector((state) => state.post.post);
   return (
     <>
-      <HomeHeader bigTitle={post?.title} smallTitle={post?.author} />
+      <HomeHeader bigTitle={postData?.title} smallTitle={postData?.author} />
       <div>
         <article className="mb-4">
           <div className="container px-4 px-lg-5">
             <div className="row gx-4 gx-lg-5 justify-content-center">
               <div className="col-md-10 col-lg-8 col-xl-7">
-                <p>{post?.description}</p>
+                <p>{postData?.description}</p>
               </div>
             </div>
           </div>
