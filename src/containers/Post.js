d0b21f684c6d0b05d023657fcb44b9b823d515ce
Tienducvi <ducvi0987@gmail.com>
2021-09-06 07:27:00 -0700

    lam cho detail post

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { detailInfo } from '../redux/actions/productActions';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { detailInfo } from '../redux/actions/productActions';
 import HomeHeader from './Header';
 import FooterHome from './Footer';
 
@@ -13,34 +13,24 @@ const api = axios.create({
 export default function PostDetail() {
   // eslint-disable-next-line prefer-const
   const { postId } = useParams();
-  // // eslint-disable-next-line no-console
-  // console.log(postId);
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     api.get('/').then((res) => {
-      // eslint-disable-next-line no-console
-      console.log(res);
       const { data } = res.data;
       setPosts(data);
-      // // eslint-disable-next-line no-console
-      // console.log(`data: ${res.data}`);
     });
   }, []);
   const post = posts.find((pId) => pId.id === postId);
-  dispatch(detailInfo(post));
-  // const postData = useSelector((state) => state.post.post);
-  // // eslint-disable-next-line no-console
-  // console.log(postData);
   return (
     <>
-      <HomeHeader />
+      <HomeHeader bigTitle={post?.title} smallTitle={post?.author} />
       <div>
         <article className="mb-4">
           <div className="container px-4 px-lg-5">
             <div className="row gx-4 gx-lg-5 justify-content-center">
               <div className="col-md-10 col-lg-8 col-xl-7">
-                <p>{}</p>
+                <p>{post?.description}</p>
               </div>
             </div>
           </div>
